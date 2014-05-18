3f22663f9858f87893329ac065e2b869e1673958
stockmopar <git@stockmopar.com>
2014-05-18 08:00:30 -0500

    Changed this to self in a few places

--- index.js
+++ index.js
@@ -152,14 +152,14 @@ Driver.prototype.createDevices = function(app, id, data, topic) {
         this.name = 'Nest - ' + (deviceData.name||id) + ' Current Temperature';
 
         self.on(topic, function(data) {
-			this.log.info("(Nest) CurrentTemp - Topic was triggered");
+			self.log.info("(Nest) CurrentTemp - Topic was triggered");
 			var deviceData = data.shared[id];
 			
             self.log.debug('Nest - Device ' + id + ' - Current temperature:' + deviceData.current_temperature);
             if (typeof deviceData.current_temperature == 'undefined') {
                 self.log.error('Nest - Device ' + id + '- ERROR: No Current Temperature!');
             } else {
-				this.log.info("(Nest) CurrentTemp - " + deviceData.current_temperature);
+				self.log.info("(Nest) CurrentTemp - " + deviceData.current_temperature);
                 this.emit('data', deviceData.current_temperature);
             }
         }.bind(this));
@@ -179,13 +179,13 @@ Driver.prototype.createDevices = function(app, id, data, topic) {
         this.name = 'Nest - ' + (deviceData.name||id) + ' Target Temperature';
 
         self.on(topic, function(data) {
-			this.log.info("(Nest) TargetTemp - Topic was triggered");
+			self.log.info("(Nest) TargetTemp - Topic was triggered");
 			var deviceData = data.shared[id];
             self.log.debug('Nest - Device ' + id + ' - Target temperature:' + deviceData.target_temperature);
              if (typeof deviceData.target_temperature == 'undefined') {
                 self.log.error('Nest - Device ' + id + '- ERROR: No Target Temperature!');
             } else {
-				this.log.info("(Nest) TargetTemp - " + deviceData.target_temperature);
+				self.log.info("(Nest) TargetTemp - " + deviceData.target_temperature);
                 this.emit('data', deviceData.target_temperature);
             }
         }.bind(this));
@@ -225,14 +225,14 @@ Driver.prototype.createDevices = function(app, id, data, topic) {
         this.name = 'Nest - ' + (deviceData.name||id) + ' Current Humidity';
 
         self.on(topic, function(data) {
-			this.log.info("(Nest) CurrentHumidity - Topic was triggered");
+			self.log.info("(Nest) CurrentHumidity - Topic was triggered");
 			var extraDeviceData = data.device[id];
 			
             self.log.debug('Nest - Device ' + id + ' - Current humidity:' + extraDeviceData.current_humidity);
             if (typeof extraDeviceData.current_humidity == 'undefined') {
                 self.log.error('Nest - Device ' + id + '- ERROR: No Current Humidity!');
             } else {
-				this.log.info("(Nest) CurrentHumidity - " + extraDeviceData.current_humidity);
+				self.log.info("(Nest) CurrentHumidity - " + extraDeviceData.current_humidity);
                 this.emit('data', extraDeviceData.current_humidity);
             }
         }.bind(this));
